995bc2e7e609df37341408fe253d049062f73429
lemmetry <artemkisel@yahoo.com>
2013-08-14 20:58:28 -0400

    Minor admin-site customization.

--- countpucks/webservice/admin.py
+++ countpucks/webservice/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from countpucks.webservice.models import Team, HockeyPlayer, PlayerScores, GoalieScores
 
 
+class HockeyPlayerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'sweater', 'position', 'team')
+    list_filter = ['team']
+    search_fields = ['full_name']
+
+
 admin.site.register(Team)
-admin.site.register(HockeyPlayer)
+admin.site.register(HockeyPlayer, HockeyPlayerAdmin)
 admin.site.register(PlayerScores)
 admin.site.register(GoalieScores)
