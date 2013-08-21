a6013626abeedbc309141a9f5ed498a9f73086e1
lemmetry <artemkisel@yahoo.com>
2013-08-21 13:57:00 -0400

    Added read-only field to be able to see when current record was crawled-created in db

--- countpucks/webservice/admin.py
+++ countpucks/webservice/admin.py
@@ -2,31 +2,17 @@ from django.contrib import admin
 from countpucks.webservice.models import Team, HockeyPlayer, PlayerScores, GoalieScores
 
 
-class TeamAdmin(admin.ModelAdmin):
-    ordering = ['team_name']
-
-
-class TeamListFilter(admin.SimpleListFilter):
-    title = 'Team'
-
-    parameter_name = 'team'
-
-    def lookups(self, request, model_admin):
-        teams = Team.objects.all()
-        return [(t.id, t.team_name) for t in teams.order_by('team_name')]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(team__exact=self.value())
-
-
 class HockeyPlayerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'sweater', 'position', 'team')
-    list_filter = (TeamListFilter,)
+    list_display = ('full_name', 'birthdate', 'sweater', 'position', 'team')
+    list_filter = ['team']
     search_fields = ['full_name']
 
 
-admin.site.register(Team, TeamAdmin)
+class PlayerScoresAdmin(admin.ModelAdmin):
+    readonly_fields = ['date']
+    
+
+admin.site.register(Team)
 admin.site.register(HockeyPlayer, HockeyPlayerAdmin)
-admin.site.register(PlayerScores)
+admin.site.register(PlayerScores, PlayerScoresAdmin)
 admin.site.register(GoalieScores)
