e356dd94197a2449479ac203ce97079fa9f00383
lemmetry <artemkisel@yahoo.com>
2013-09-23 21:30:47 -0400

    Added sorting, filtering and searching features to Player- and GoalieScores

--- countpucks/webservice/admin.py
+++ countpucks/webservice/admin.py
@@ -9,10 +9,20 @@ class HockeyPlayerAdmin(admin.ModelAdmin):
 
 
 class PlayerScoresAdmin(admin.ModelAdmin):
+    list_display = ('get_fullname', 'get_team', 'get_position', 'date')
+    list_filter = ['player__team']
+    search_fields = ['player__full_name']
     readonly_fields = ['date']
+
+
+class GoalieScoresAdmin(admin.ModelAdmin):
+    list_display = ('get_fullname', 'get_team', 'date')
+    list_filter = ['player__team']
+    search_fields = ['player__full_name']
+    readonly_fields = ['date']    
     
 
 admin.site.register(Team)
 admin.site.register(HockeyPlayer, HockeyPlayerAdmin)
 admin.site.register(PlayerScores, PlayerScoresAdmin)
-admin.site.register(GoalieScores)
+admin.site.register(GoalieScores, GoalieScoresAdmin)
